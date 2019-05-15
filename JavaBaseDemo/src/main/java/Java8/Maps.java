c8b20c1f5b24691f5ea089173cf6779051c155c4
wbin <33045209+wbin327@users.noreply.github.com>
2019-05-16 02:35:36 +0800

    修改：Java 8,Maps文件新增一个Map排序的方法

--- JavaBaseDemo/src/main/java/Java8/Maps.java
+++ JavaBaseDemo/src/main/java/Java8/Maps.java
@@ -1,15 +1,15 @@
 package Java8;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 /**
  * java 8, Map
  * Map 类型不支持 streams，不过Map提供了一些新的有用的方法来处理一些日常任务。
- * Map接口本身没有可用的 stream（）方法，但是你可以在键，值上创建专门的流或者通过 map.keySet().stream(),map.values().stream()和map.entrySet().stream()。
+ * Map接口本身没有可用的 stream（）方法，但是你可以在键，值上创建专门的流或者通过 map.keySet().stream(),map.values().stream()
+ * map.entrySet().stream()获取map的流，entry的结构就是一个键值对
  */
 public class Maps {
     static Map<Integer, String> map = new HashMap<>();
@@ -23,7 +23,7 @@ public class Maps {
     /**
      * 遍历map
      */
-    static void foreach(){
+    static void foreach(Map map){
         map.forEach((key, value) -> System.out.println(String.format("key: %s;   value: %s", key, value)));
     }
 
@@ -40,7 +40,7 @@ public class Maps {
      */
     static void remove(){
         map.remove(1, "value1");
-        foreach();
+        foreach(Maps.map);
     }
 
     /**
@@ -50,9 +50,25 @@ public class Maps {
         System.out.println(map.getOrDefault(20, "not found"));
     }
 
+    /**
+     * 根据key倒叙排序
+     * 使用map.entrySet().stream()
+     */
+    static void sort(){
+        System.out.println("原Map中元素的顺序为：");
+        foreach(Maps.map);
+        System.out.println("根据key倒叙排序后的顺序为：");
+        List<Map.Entry<Integer, String>> list = map.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByKey((a, b) -> b-a))
+                .collect(Collectors.toList());
+                //.collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+        list.forEach(System.out::println);
+    }
+
     public static void main(String[] args){
-        foreach();
         select();
         getDefault();
+        sort();
     }
 }
